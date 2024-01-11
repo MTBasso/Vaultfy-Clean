226734be0f3a010e0d57b026932e2e44c02cee48
Matheus Basso <matheusbasso@protonmail.com>
2024-01-11 12:19:58 -0300

    config: fixed PORT

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -11,7 +11,7 @@ import { router } from './routes';
 import '../../container';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(json());
 app.use(cookieParser());

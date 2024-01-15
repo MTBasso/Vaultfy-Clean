8773a69275fc6ccf476c6f9f03a27a3097b11fb1
Matheus Basso <matheusbasso@protonmail.com>
2024-01-15 13:32:27 -0300

    fix: fixed last commit

--- src/shared/infra/http/routes/auth.routes.ts
+++ src/shared/infra/http/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { celebrate } from 'celebrate';
 import { Router } from 'express';
 
+import { ListUserVaultsController } from '../../../../modules/user/useCases/listUserVaults/list-user-vaults.controller';
 import { listUserVaultsValidator } from '../../../../modules/user/useCases/listUserVaults/validator';
 import { RegisterUserController } from '../../../../modules/user/useCases/registerUser/register-user.controller';
 import { registerValidator } from '../../../../modules/user/useCases/registerUser/validator';
@@ -12,6 +13,7 @@ const authRoutes = Router();
 
 const registerUserController = new RegisterUserController();
 const userLoginController = new UserLoginController();
+const listUserVaultsController = new ListUserVaultsController();
 
 authRoutes.post('/register', celebrate(registerValidator), registerUserController.handle);
 authRoutes.post('/login', celebrate(loginValidator), userLoginController.handle);
@@ -19,7 +21,7 @@ authRoutes.get(
   '/fetch/:id',
   celebrate({ params: listUserVaultsValidator }),
   authenticateToken,
-  userLoginController.handle
+  listUserVaultsController.handle
 );
 
 export { authRoutes };

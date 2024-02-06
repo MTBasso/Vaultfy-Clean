416ba36e38dcd31f9b70d804f974b2ccb0d35601
Matheus Basso <matheusbasso@protonmail.com>
2024-02-06 10:50:33 -0300

    removed celebrateErrors.ts

--- src/utils/celebrateErrors.ts
+++
@@ -1,8 +0,0 @@
-const customMessage = (field: string) => {
-  return {
-    'string.empty': `The field ${field} can't be empty!`,
-    'any.required': `The ${field} field is required!`
-  };
-};
-
-export { customMessage };
